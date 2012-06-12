53334f564bc8a0ab40f9f6ff5ab82b092f8d8c78
Alexander Weinitschke <aweinitschke@fh-bielefeld.de>
2012-06-12 15:01:53 +0200

    KeyEvent hinzugeügt für Live Syntaxcheck (gerüst)

---
+++ SyntaxHighlighter/src/LiveHandler.java
@@ -0,0 +1,28 @@
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+
+
+public class LiveHandler implements KeyListener{
+	
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		
+		
+	}
+
+
+}
