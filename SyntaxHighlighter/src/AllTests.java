8bb9495bafa51ce8db2dc1153f48d647cdeccb24
wtwe <wtwet>
2012-06-19 14:54:15 +0200

    Frogger gestartet

---
+++ SyntaxHighlighter/src/AllTests.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+@RunWith(Suite.class)
+@SuiteClasses({ LexerTest.class, PublicMethodTest.class })
+public class AllTests {
+
+}
